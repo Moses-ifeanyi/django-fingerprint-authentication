4d5b8e7f1fa7ca576202299203ceb7ffa4620c1d
Moses Ifeanyi <moses.ifeanyim@gmail.com>
2022-10-22 19:53:02 +0100

    configured heroku domain

--- web_fingerprint_authn/settings.py
+++ web_fingerprint_authn/settings.py
@@ -153,8 +153,8 @@ if DEBUG:
     FIDO_SERVER_ID=u"localehost"      # Server rp id for FIDO2, it is the full domain of your project
     
 else:
-    U2F_APPID="https://django-fingerprint-authn.herokuapp.com"    #URL For U2F
-    FIDO_SERVER_ID=u"django-fingerprint-authn.herokuapp.com" 
+    U2F_APPID="https://django-fingerprint-auth.herokuapp.com"    #URL For U2F
+    FIDO_SERVER_ID=u"django-fingerprint-auth.herokuapp.com" 
     
 FIDO_SERVER_NAME=u"web_based_fingerprint_authn"
 MFA_REDIRECT_AFTER_REGISTRATION = 'accounts:index'
