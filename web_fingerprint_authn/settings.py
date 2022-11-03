bfeaff165fcd66914ab656fb44296143fb602005
Moses Ifeanyi <moses.ifeanyim@gmail.com>
2022-11-03 12:51:31 +0100

    changes to domain name

--- web_fingerprint_authn/settings.py
+++ web_fingerprint_authn/settings.py
@@ -149,8 +149,8 @@ RECOVERY_ITERATION = 350000 #Number of iteration for recovery code, higher is mo
 TOKEN_ISSUER_NAME="web_based_fingerprint_authn"      #TOTP Issuer name
 
 if DEBUG: 
-    U2F_APPID="https://django-fingerprint-auth.herokuapp.com"    #URL For U2F
-    FIDO_SERVER_ID=u"django-fingerprint-auth.herokuapp.com" 
+    U2F_APPID="https://web-fingerprint-auth.herokuapp.com"    #URL For U2F
+    FIDO_SERVER_ID=u"web-fingerprint-auth.herokuapp.com" 
 else:
     U2F_APPID="https://localhost"    #URL For U2F
     FIDO_SERVER_ID=u"localhost"   # Server rp id for FIDO2, it is the full domain of your project
